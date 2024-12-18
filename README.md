5e3735db870776e578b7e74e2f37c0cb78316893
vinay <devaragarivinayyadav7@gmail.com>
2024-12-18 07:44:18 +0000

    commit

---
+++ README.md
@@ -0,0 +1,36 @@
+
+# Book Management System
+
+A simple **Book Management System** built using **React.js** and **Axios** to handle CRUD operations for managing a collection of books. This app allows users to add, update, delete, and search for books based on the author's name.
+
+## Features
+
+- **Add Books**: Users can add a new book to the system by providing details such as title, author, genre, pages, and published date.
+- **Update Books**: Users can update the details of an existing book by clicking the "Edit" button and modifying the book's data.
+- **Delete Books**: Users can delete a book from the system by clicking the "Delete" button next to each book.
+- **Search**: Users can filter books based on the author's name using a search bar.
+- **Responsive Design**: The app is fully responsive and works seamlessly on mobile, tablet, and desktop devices.
+
+## Technologies Used
+
+- **React.js**: Frontend framework to build the user interface.
+- **Axios**: To perform HTTP requests (GET, POST, PUT, DELETE) for CRUD operations.
+- **CSS**: Custom CSS for styling and ensuring the app is responsive across devices.
+
+## Prerequisites
+
+To run this project locally, you need to have the following installed on your system:
+
+- **Node.js** (v14 or later)
+- **npm** or **yarn** (Node package manager)
+
+## Installation
+
+Follow these steps to get the project running on your local machine:
+
+1. **Clone the repository**:
+
+   ```bash
+   git clone https://github.com/your-username/book-management-system.git
+   cd book-management-system
+
---
+++ src/App.js
@@ -0,0 +1,223 @@
+import React, {useState, useEffect} from 'react'
+import axios from 'axios'
+import {Oval} from 'react-loader-spinner'
+import './App.css'
+
+function App() {
+  const [books, setBooks] = useState([])
+  const [newBook, setNewBook] = useState({
+    title: '',
+    author: '',
+    genre: '',
+    pages: '',
+    publishedDate: '',
+  })
+  const [search, setSearch] = useState('')
+  const [editing, setEditing] = useState(false)
+  const [currentBook, setCurrentBook] = useState(null)
+  const [loading, setLoading] = useState(true)
+  useEffect(() => {
+    fetchBooks()
+  }, [])
+
+  const fetchBooks = async () => {
+    setLoading(true)
+    try {
+      const response = await axios.get(
+        'https://book-mangement-w1ri.onrender.com/books',
+      )
+      setBooks(response.data)
+      setLoading(false)
+    } catch (error) {
+      console.error('Error fetching books', error)
+    }
+  }
+
+  const handleChange = e => {
+    const {name, value} = e.target
+    setNewBook(prev => ({
+      ...prev,
+      [name]: value,
+    }))
+  }
+
+  const handleSearch = e => {
+    setSearch(e.target.value)
+    
+  }
+
+  const handleAddBook = async e => {
+    setLoading(true)
+    e.preventDefault()
+    try {
+      await axios.post(
+        'https://book-mangement-w1ri.onrender.com/books',
+        newBook,
+      )
+      fetchBooks()
+      setLoading(false)
+      setNewBook({
+        title: '',
+        author: '',
+        genre: '',
+        pages: '',
+        publishedDate: '',
+      })
+    } catch (error) {
+      console.error('Error adding book', error)
+    }
+  }
+
+  const handleEditBook = async e => {
+    e.preventDefault()
+    setLoading(true)
+    try {
+      await axios.put(
+        `https://book-mangement-w1ri.onrender.com/books/${currentBook.bookId}`,
+        newBook,
+      )
+      fetchBooks()
+      setEditing(false)
+      setLoading(false)
+      setNewBook({
+        title: '',
+        author: '',
+        genre: '',
+        pages: '',
+        publishedDate: '',
+      })
+    } catch (error) {
+      console.error('Error updating book', error)
+    }
+  }
+
+  const handleDeleteBook = async bookId => {
+    setLoading(true)
+    try {
+      await axios.delete(
+        `https://book-mangement-w1ri.onrender.com/books/${bookId}`,
+      )
+      fetchBooks()
+      setLoading(false)
+    } catch (error) {
+      console.error('Error deleting book', error)
+    }
+  }
+
+  const handleEditForm = book => {
+    setLoading(true)
+    setEditing(true)
+    setCurrentBook(book)
+    setNewBook({
+      title: book.title,
+      author: book.author,
+      genre: book.genre,
+      pages: book.pages,
+      publishedDate: book.publishedDate,
+    })
+    setLoading(false)
+  }
+
+  const filteredBooks = books.filter(book =>
+    book.author.toLowerCase().includes(search.toLowerCase()),
+  )
+
+  return (
+    <div className="app-container">
+      <h1>Book Management</h1>
+
+      <div className="search-container">
+        <input
+          type="search"
+          placeholder="Search by Author"
+          value={search}
+          onChange={handleSearch}
+        />
+      </div>
+
+      <div className="form-container">
+        <h2>{editing ? 'Edit Book' : 'Add New Book'}</h2>
+        <form onSubmit={editing ? handleEditBook : handleAddBook}>
+          <input
+            type="text"
+            name="title"
+            placeholder="Title"
+            value={newBook.title}
+            onChange={handleChange}
+            required
+          />
+          <input
+            type="text"
+            name="author"
+            placeholder="Author"
+            value={newBook.author}
+            onChange={handleChange}
+            required
+          />
+          <input
+            type="text"
+            name="genre"
+            placeholder="Genre"
+            value={newBook.genre}
+            onChange={handleChange}
+            required
+          />
+          <input
+            type="number"
+            name="pages"
+            placeholder="Pages"
+            value={newBook.pages}
+            onChange={handleChange}
+            required
+          />
+          <input
+            type="number"
+            name="publishedDate"
+            value={newBook.publishedDate}
+            placeholder="Published Date"
+            onChange={handleChange}
+            required
+          />
+          <button type="submit">{editing ? 'Update' : 'Add'}</button>
+        </form>
+      </div>
+      {loading ? (
+        <div className="loading">
+          <Oval
+            visible={true}
+            height="80"
+            width="80"
+            color="#4fa94d"
+            ariaLabel="oval-loading"
+            wrapperStyle={{}}
+            wrapperClass=""
+          />
+        </div>
+      ) : (
+        <div className="books-list">
+          {filteredBooks.length > 0 ? (
+            filteredBooks.map(book => (
+              <div className="book-item" key={book.bookId}>
+                <h3>{book.title}</h3>
+                <p>Author: {book.author}</p>
+                <p>Genre: {book.genre}</p>
+                <p>Pages: {book.pages}</p>
+                <p>Published: {book.publishedDate}</p>
+                <div className="actions">
+                  <button onClick={() => handleEditForm(book)}>Edit</button>
+                  <button onClick={() => handleDeleteBook(book.bookId)}>
+                    Delete
+                  </button>
+                </div>
+              </div>
+            ))
+          ) : (
+            <h2 className="text-center">No books found</h2>
+          )}
+        </div>
+      )}
+    </div>
+  )
+}
+
+export default App
